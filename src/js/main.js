6bcd8a07d3e5e0cef0eaba56dd8baa8fab00488e
NeedHate <need_hate@mail.ru>
2018-02-04 19:22:17 +0200

    some previous updates

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,10 @@
-//= other.js
 //= menu.js
 //= header.js
 //= weather.js
 
 const base = {
     _header() {
-        const header = document.querySelectorAll('.header');
-
-        header.forEach((obj) => {
-            new Header(obj);
-        });
+        new Header('.header');
     },
     _menu() {
         const menu = document.querySelectorAll('.menu');
@@ -18,13 +13,9 @@ const base = {
             new Menu(obj);
         });
     },
-    _script() {
-        console.log('...script is working');
-    },
     init() {
         this._header();
         this._menu();
-        this._script();
     }
 }
 
