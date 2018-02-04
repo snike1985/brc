d547a556d3ac81133dd9f6b86e71e36a38ad5dfa
NeedHate <need_hate@mail.ru>
2018-02-04 19:48:57 +0200

    JS bundle fix

--- src/js/main.js
+++ src/js/main.js
@@ -13,9 +13,31 @@ const base = {
             new Menu(obj);
         });
     },
+
+    _parallax() {
+        const timeline = new TimelineMax();
+        const paralaxElem = document.getElementById('hero-title');
+
+        timeline.to(paralaxElem, 1, {y: 100});
+
+        const controller = new ScrollMagic.Controller();
+        const scene = new ScrollMagic.Scene({
+            duration: '50%', // duration in px eg. 300, 0 = autoplay
+            // duration: '100%', // resposive duration in %
+            offset: 100, // offset trigger position by 100px
+            triggerElement: '#hero', // what will trigger scene
+            triggerHook: 0
+        });
+        // scene.setTween(timeline);
+        scene.addIndicators({ name: 'Blah Bla Bla' });
+        scene.setTween(timeline);
+        // scene.setPin('.hero');
+        scene.addTo(controller);
+    },
     init() {
         this._header();
         this._menu();
+        this._parallax();
     }
 }
 
