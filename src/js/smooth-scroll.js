c5acc0ceb0e045dea4cee3403688bcf87c9daf96
NeedHate <need_hate@mail.ru>
2018-02-08 14:12:34 +0200

    Current progress, almost done with page-4

--- src/js/smooth-scroll.js
+++ src/js/smooth-scroll.js
@@ -1,11 +1,11 @@
 $(function(){
-	var _window = $( window ),
+	var _window = $(window),
 		scrollTime = 0.5,
 		scrollDistance = 200;
 
     _window.on("mousewheel DOMMouseScroll", function(event){
         event.preventDefault();
-        
+
         if ( !$('.menu-active').length) {
             var delta = event.originalEvent.wheelDelta/120 || -event.originalEvent.detail/3,
                 scrollTop = _window.scrollTop(),
